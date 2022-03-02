96ab527f0eaa09cb3020db92c47a6983d1f1ffd3
doma-itachi <itachips@yahoo.co.jp>
2022-03-02 19:18:46 +0900

    リンク修正

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 window.onload=function(){
     document.getElementById("description").innerHTML=chrome.i18n.getMessage("ext_desc");
     document.getElementById("version").innerHTML="v"+chrome.runtime.getManifest().version;
-    document.getElementById("btn_repo").addEventListener("click",function(){window.open("https://www.github.com","_blank")});
+    document.getElementById("btn_repo").addEventListener("click",function(){window.open("https://github.com/doma-itachi/Youtube-shorts-block","_blank")});
 };
