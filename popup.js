6ff9966e4f48b881c3c865614328b4fc681e938e
doma-itachi <itachips@yahoo.co.jp>
2022-03-08 18:33:34 +0900

    トグルボタン実装

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
+debug=false;
+
 window.onload=function(){
     document.getElementById("description").innerHTML=chrome.i18n.getMessage("ext_desc");
     document.getElementById("version").innerHTML="v"+chrome.runtime.getManifest().version;
-    document.getElementById("btn_repo").addEventListener("click",function(){window.open("https://github.com/doma-itachi/Youtube-shorts-block","_blank")});
+    // document.getElementById("btn_repo").addEventListener("click",function(){window.open("https://github.com/doma-itachi/Youtube-shorts-block","_blank")});
+    document.getElementById("toggle_wrap").addEventListener("click",function(){
+            debug=!debug;
+            document.getElementById("toggle_wrap").setAttribute("checked", debug);
+            document.getElementById("toggle_circle").setAttribute("checked",debug);
+    });
 };
