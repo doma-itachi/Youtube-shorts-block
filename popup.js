3c6ff62a2112c748690c7480d55672213d9d7f6f
doma-itachi <itachips@yahoo.co.jp>
2022-03-25 19:30:36 +0900

    innerHTML to textContent

--- popup.js
+++ popup.js
@@ -34,16 +34,16 @@ window.onload=function(){
     });
 
     //set i18n
-    document.getElementById("description").innerHTML=chrome.i18n.getMessage("ext_desc");
-    document.querySelector("#hideShortTabInput + label").innerHTML=chrome.i18n.getMessage("cfg_hide_tab");
-    document.querySelector("#hideShortVideoInput + label").innerHTML=chrome.i18n.getMessage("cfg_hide_video");
-    document.getElementById("warn_col").innerHTML=chrome.i18n.getMessage("cfg_warn_col");
-    document.getElementById("warn_msg").innerHTML=chrome.i18n.getMessage("cfg_warn_msg");
-    document.getElementById("warn_fb").innerHTML=chrome.i18n.getMessage("cfg_warn_fb");
-    document.getElementById("warn_or").innerHTML=chrome.i18n.getMessage("cfg_warn_or");
-    document.getElementById("warn_pr").innerHTML=chrome.i18n.getMessage("cfg_warn_pr");
+    document.getElementById("description").textContent=chrome.i18n.getMessage("ext_desc");
+    document.querySelector("#hideShortTabInput + label").textContent=chrome.i18n.getMessage("cfg_hide_tab");
+    document.querySelector("#hideShortVideoInput + label").textContent=chrome.i18n.getMessage("cfg_hide_video");
+    document.getElementById("warn_col").textContent=chrome.i18n.getMessage("cfg_warn_col");
+    document.getElementById("warn_msg").textContent=chrome.i18n.getMessage("cfg_warn_msg");
+    document.getElementById("warn_fb").textContent=chrome.i18n.getMessage("cfg_warn_fb");
+    document.getElementById("warn_or").textContent=chrome.i18n.getMessage("cfg_warn_or");
+    document.getElementById("warn_pr").textContent=chrome.i18n.getMessage("cfg_warn_pr");
 
-    document.getElementById("version").innerHTML="v"+chrome.runtime.getManifest().version;
+    document.getElementById("version").textContent="v"+chrome.runtime.getManifest().version;
     document.getElementById("more_icon").addEventListener("click", function(){
         let elem=document.getElementById("more_checkbox");
         elem.checked=!elem.checked;
