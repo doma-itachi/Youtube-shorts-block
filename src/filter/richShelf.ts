575508ba2dfd4500274eeac9eabdde9a61714f54
itachi <94786908+doma-itachi@users.noreply.github.com>
2024-09-18 18:45:03 +0900

    Fix block the entire News(and other) page #50

--- src/filter/richShelf.ts
+++ src/filter/richShelf.ts
@@ -1,5 +1,5 @@
 export function richShelfFilter() {
-    const shelfs = document.querySelectorAll("ytd-rich-shelf-renderer");
+    const shelfs = document.querySelectorAll("ytd-rich-shelf-renderer:has(h2>yt-icon:not([hidden]))");
     for (const shelf of shelfs) {
         shelf.remove();
     }
